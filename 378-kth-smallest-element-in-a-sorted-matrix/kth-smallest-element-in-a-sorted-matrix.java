dfa1f669d7847789d6b6de4d454f463c10b58dfe
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-08-13 15:49:23 +0530

    Time: 10 ms (63.03%) | Memory: 49.9 MB (34.84%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,19 +1,36 @@
 class Solution {
-    public int kthSmallest(int[][] a, int k) {
-        
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+    private int fun(int[][] a, int m) {
+        int ans = 0;
         for(int i = 0; i < a.length; i++) {
-            for(int j = 0; j < a[i].length; j++) {
-                map.put(a[i][j], map.getOrDefault(a[i][j], 0) + 1);
+            int low = 0, high = a[0].length-1;
+            int a1 = 0;
+            while(low <= high) {
+                int mid = (low + high) / 2;
+                if(a[i][mid] < m) {
+                    a1 = mid + 1;
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+             ans += a1;
+        }
+        return ans;
+    }
+    public int kthSmallest(int[][] a, int k) {
+        int n = a.length;
+        int m = a[0].length;
+        int low = a[0][0], high = a[n-1][m-1];
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            int cnt = fun(a, mid);
+            System.out.print(cnt+" ");
+            if(cnt < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        int res = 0;
-        for(int key : map.keySet()) {
-            if(k > 0) {
-                res = key;
-                k -= map.get(key);
-            }
-        }
-        return res;
+        return high;
     }
 }

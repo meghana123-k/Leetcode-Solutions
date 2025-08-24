e23d449ccee34bb8adc3a0040fcd95c5ba37f5c7
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-08-24 18:42:15 +0530

    Time: 1 ms (99.74%) | Memory: 45.9 MB (47.20%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num : nums) {
+            xor ^= num;
+        }
+        xor = ((xor & (xor-1)) ^ xor);
+        int bx1 = 0;
+        int bx2 = 0;
+        for(int num : nums) {
+            if((num | xor) == num) {
+                bx1 ^= num;
+            } else {
+                bx2 ^= num;
+            }
+        }
+        return new int[]{bx1, bx2};
+    }
+}

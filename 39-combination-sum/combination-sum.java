eb8a0539b75d971adcc079c8d71e70035e2847ba
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-08-29 18:24:17 +0530

    Time: 1 ms (99.90%) | Memory: 44.9 MB (30.92%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] a, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        combSum(a, 0, k, res, new ArrayList<>());
+        return res;
+    }
+    private void combSum(int[] a, int idx, int k, List<List<Integer>> res, List<Integer> ar) {
+        if(idx == a.length) {
+            if(k == 0) {
+                res.add(new ArrayList<>(ar));
+            }
+            return;
+        }
+        if(a[idx] <= k) {
+            ar.add(a[idx]);
+            combSum(a, idx, k-a[idx], res, ar);
+            ar.remove(ar.size() - 1);
+        }
+        combSum(a, idx + 1, k, res, ar);
+    }
+}

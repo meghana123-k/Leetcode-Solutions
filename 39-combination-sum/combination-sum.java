eabe3ff2db095928827d2ee03c32769be4199332
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-09-26 13:31:35 +0530

    Time: 1 ms (99.88%) | Memory: 44.6 MB (64.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,34 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] a, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        combSum(a, 0, k, res, new ArrayList<>());
-        return res;
-    }
-    private void combSum(int[] a, int idx, int k, List<List<Integer>> res, List<Integer> ar) {
-        if(idx == a.length) {
-            if(k == 0) {
-                res.add(new ArrayList<>(ar));
-            }
+    public void combSum(int[] a, int idx, int k, List<List<Integer>> res, List<Integer> ar) {
+        // if(idx == a.length) {
+        //     if(k == 0) {
+        //         res.add(new ArrayList<>(ar));
+        //     }
+        //     return;
+        // }
+        // if(a[idx] <= k) {
+        //     ar.add(a[idx]);
+        //     combSum(a, idx, k-a[idx], res, ar);
+        //     ar.remove(ar.size() - 1);
+        // }
+        // combSum(a, idx + 1, k, res, ar);
+        if(idx < 0) return ;
+        if(k == 0) {
+            res.add(new ArrayList<>(ar));
             return;
         }
+        combSum(a, idx-1, k, res, ar);
         if(a[idx] <= k) {
             ar.add(a[idx]);
-            combSum(a, idx, k-a[idx], res, ar);
-            ar.remove(ar.size() - 1);
+            combSum(a, idx, k - a[idx], res, ar);
+            ar.remove(ar.size()-1);
         }
-        combSum(a, idx + 1, k, res, ar);
+       
+    }
+    public List<List<Integer>> combinationSum(int[] a, int k) {
+         List<List<Integer>> res = new ArrayList<>();
+
+        combSum(a, a.length-1, k, res, new ArrayList<>());
+        return res;
     }
 }

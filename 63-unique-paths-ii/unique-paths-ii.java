56b5605e61b44e36aacf8400658ec7397072d49a
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2026-01-10 10:50:18 +0530

    Time: 1 ms (17.85%) | Memory: 43.6 MB (26.87%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,50 +1,54 @@
 class Solution {
-    int[][] dp;
-    public int helper(int[][] grid, int n, int m) {
-        if(n == 0 || m == 0) return 1;
-        // if(n < 0 || m < 0) return 0;
-        // if(grid[m][0] == 1 || grid[0][n] == 1) return 0;
-        if(dp[n][m] != -1) return dp[n][m];
-        int up = 0, left = 0;
-        if(grid[n-1][m] != 1)  {
-            up = helper(grid, n-1, m);
-        }
-        if(grid[n][m-1] != 1) {
-            left = helper(grid, n, m-1);
-        }
-        return dp[n][m] = up + left;
-    }
     public int uniquePathsWithObstacles(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        dp = new int[n][m];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return 0;
+        if(n == 0 || m == 0) return 0;
+        // if(n == 1 || m == 1) return 1;
+        int[][] dp = new int[n][m];
+
         int i = 0;
         while(i < n) {
-            if(grid[i][0] == 1) {
-                break;
-            }
+            if(grid[i][0] == 1) break;
+            dp[i][0] = 1;
             i++;
         }
-        while(i < n) {
-            grid[i][0] = 1;
-            i++;
-        }
-        
         int j = 0;
         while(j < m) {
-            if(grid[0][j] == 1) {
-                break;
-            }
+            if(grid[0][j] == 1) break;
+            dp[0][j] = 1;
             j++;
+        }
+        while(i < n) {
+            grid[i][0] = -1;
+            i++;
         }
         while(j < m) {
-            grid[0][j] = 1;
+            grid[0][j] = -1;
             j++;
         }
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return 0;
-        return helper(grid, n-1, m-1);
+        for(int k = 1; k < n; k++) {
+            for(int l = 1; l < m; l++) {
+                if(grid[k][l] == 1) {
+                    dp[k][l] = -1;
+                }
+            }
+        }
+        for(int k = 1; k < n; k++) {
+            for(int l = 1; l < m; l++) {
+                if(dp[k][l] != -1) {
+                    int up = 0, down = 0;
+                    if(dp[k-1][l] != -1) {
+                        up = dp[k-1][l];
+                    }
+                    if(dp[k][l-1] != -1) {
+                        down = dp[k][l-1];
+                    }
+                    dp[k][l] = up + down;
+                    // System.out.println(dp[k-1][l]+" "+dp[k][l-1]);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }

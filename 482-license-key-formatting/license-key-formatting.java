1f1e445f0c95aea43a40d0d8c6ca4d54f76bc237
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2026-02-26 23:15:16 +0530

    Time: 11 ms (66.78%) | Memory: 46.1 MB (95.65%) - LeetSync

---
+++ 482-license-key-formatting/license-key-formatting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        int count = 0;
+        int n = s.length();
+        if(n == 0) return "";
+        StringBuilder ans = new StringBuilder();
+        for(int i = n-1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if(ch != '-') {
+                ans.append(Character.toUpperCase(ch));
+                count++;
+                if(count == k) {
+                    ans.append('-');
+                    count = 0;
+                }
+            }
+        }
+        if(ans.length() > 0 && ans.charAt(ans.length()-1) == '-'){
+            ans = new StringBuilder(ans.substring(0, ans.length()-1));
+        } 
+        ans.reverse();
+        return ans.toString();
+    }
+}
